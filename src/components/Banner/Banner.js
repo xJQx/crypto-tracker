aa43d5113b941f34214f0f9be7df4622553e8207
Jing Qiang <tohjingqiang@gmail.com>
2021-12-05 22:39:40 +0800

    banner

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,57 @@
+import { Container, makeStyles, Typography } from '@material-ui/core';
+import React from 'react';
+
+const useStyles = makeStyles(() => ({
+    banner: {
+        backgroundImage: "url(./banner2.jpg)"
+    },
+    bannerContent: {
+        height: 400,
+        display: "flex",
+        flexDirection: "column",
+        paddingTop: 25,
+        justifyContent: "space-around"
+    },
+    tagline: {
+        display: "flex",
+        height: "40%",
+        flexDirection: "column",
+        justifyContent: "center",
+        textAlign: "center"
+    }
+}))
+
+const Banner = () => {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.banner}>
+            <Container className={classes.bannerContent}>
+                <div className={classes.tagline}>
+                    <Typography
+                        variant = "h2"
+                        style={{
+                            fontWeight: "bold",
+                            marginBottom: 15,
+                            fontFamily: "Montserrat"
+                        }}
+                    >
+                        Crypto Tracker
+                    </Typography>
+                    <Typography
+                        variant = "subtitle2"
+                        style={{
+                            color: "darkgrey",
+                            textTransform: "capitalize",
+                            fontFamily: "Montserrat"
+                        }}
+                    >
+                        Track your favorite Crypto Currency Prices!
+                    </Typography>
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+export default Banner
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Banner from '../components/Banner/Banner'
 
 const Homepage = () => {
     return (
         <div>
-            HomePage
+            <Banner />
         </div>
     )
 }
