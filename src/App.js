0c5c7d9f07a35ba2714e8aefb48430d4c2f1b4e0
Jing Qiang <tohjingqiang@gmail.com>
2021-12-06 20:02:46 +0800

    url changes

--- src/App.js
+++ src/App.js
@@ -22,8 +22,8 @@ function App() {
       <div className={classes.App}>
         <Header />
         <Routes>
-          <Route path="/" element={<Homepage />} exact />
-          <Route path="/coins/:id" element={<Coinpage />} />
+          <Route path="/crypto-tracker" element={<Homepage />} exact />
+          <Route path="/crypto-tracker/coins/:id" element={<Coinpage />} />
         </Routes>
       </div>
     </BrowserRouter>
